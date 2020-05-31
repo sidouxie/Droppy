2dfb076041f122aca1b1fc23559a51a71a33241b
sidouxie <sid_ouxiee@hotmail.com>
2020-06-01 00:31:46 +0100

    ajout sass, optimisation des titre acceuil, effect scroll animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/sheet.css'
+import '../styles/style.scss'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -32,11 +32,29 @@ export default ({ filmes }) =>
             <div className="image-fluid">
                 <div id='bgImage' className="img-fluid"></div>
                 <div className="sectionText">
-                    <h1>Droppy site streaming gratuit</h1>
+                    <h1>Droppy site streaming en VF gratuit</h1>
+                    <h2>Regardez tout vos Filmes, séries, animes préférer gratuitement.</h2>
                 </div>
+                <div className="scroll"></div>
             </div>
         </div>
-        
+
+
+        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                <img className="d-block w-50" src="https://colorlib.com/wp/wp-content/uploads/sites/2/bootstrap-4-carousel-800x547.jpg" alt="First slide" />
+                </div>
+                <div className="carousel-item">
+                <img className="d-block w-50" src="https://colorlib.com/wp/wp-content/uploads/sites/2/bootstrap-4-carousel-800x547.jpg" alt="Second slide" />
+                </div>
+                <div className="carousel-item">
+                <img className="d-block w-50" src="https://colorlib.com/wp/wp-content/uploads/sites/2/bootstrap-4-carousel-800x547.jpg" alt="Third slide" />
+                </div>
+                </div>           
+        </div>  
+
+
        <div className="container">
             <div className="row">
             <div>
