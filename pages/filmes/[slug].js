e0ccffc3ae22736ddfa1c1f4b8da045f713827b1
sidouxie <sid_ouxiee@hotmail.com>
2020-06-08 19:31:46 +0100

    amélioration de la page slug avec l aspect responsive

--- pages/filmes/[slug].js
+++ pages/filmes/[slug].js
@@ -57,16 +57,21 @@ export default ({ filme }) => (
     <React.Fragment>
         <Layout>
             <Head><title>Droppy - Regardez {filme.title} en VF streaming gratuit.</title></Head>
+            <div className="bg-hero" >
+                <img src={filme.cover.url} alt={filme.cover.fileName} />
+            </div>
             <div className="container">
                 <div className="row">
                     <div className="section-info">
-                    <h1> {filme.title} </h1>
+                        <div className="section-one">
+                        <h2> {filme.title} </h2>
                 <p><strong>Catégorie</strong> : {filme.category} </p>
                 <p><strong>Réalisateur</strong> :  {filme.scenario} </p>
                 <p><strong>Durée du filme</strong> :  {filme.duree} Min. </p>
                 <p><strong>Résumé</strong> :  {filme.description} </p>
                 <p><strong>Année de sortie</strong> :  {filme.year} </p>
-                <img src={filme.cover.url} alt={filme.cover.fileName} />
+                    </div>
+                    <div className="section-two"><img src={filme.cover.url} alt={filme.cover.fileName} /></div>
                 </div>
                 </div>
                 </div>
