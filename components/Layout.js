d7de73f82f8caaa5c88f31df6a539d434b367168
sidouxie <sid_ouxiee@hotmail.com>
2020-06-09 17:47:36 +0100

    optimisation responsive css

--- components/Layout.js
+++ components/Layout.js
@@ -10,9 +10,10 @@ const Layout = ( props ) => {
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
             </Head>
 
+            <div className='min-Width'>
             <Nav />
-
             { props.children }
+            </div>
             
         </>
     )
