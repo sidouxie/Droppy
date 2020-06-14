76f861824f3d6ff1729c517b7cefba73df4a38e2
sidouxie <sid_ouxiee@hotmail.com>
2020-06-14 15:43:01 +0100

    .

--- pages/filmes/[slug].js
+++ pages/filmes/[slug].js
@@ -85,8 +85,8 @@ export default ({ filme }) => (
                     </div>
                 </div>
                 <div className="section-vid">
-                        <iframe style={{width: "100%"}} src={`https://embed.mystream.to/${filme.apiurl}`} scrolling="no" frameborder="0" width="700" height="420" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>    
-                    </div>
+                    <iframe style={{width: "100%"}} src={`https://embed.mystream.to/${filme.apiurl}`} scrolling="no" frameborder="0" width="700" height="420" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>    
+                </div>
             </div>
         </Layout>
         </React.Fragment>
