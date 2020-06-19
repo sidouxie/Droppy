0eb519921ae77475cb7881655e2eb776d71b7016
sidouxie <sid_ouxiee@hotmail.com>
2020-06-19 23:48:16 +0100

    .

--- pages/filmes.js
+++ pages/filmes.js
@@ -6,6 +6,7 @@ function filmes() {
             <div className="container-fluid">
                 <div className="row">
                     <h1>Bienvenue sur la page Filmes</h1>
+                    <p>push some data !</p>
                 </div>
             </div>
         </Layout>
