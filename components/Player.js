2564f6b05753ab6afa517be74593c8fcba7a56d1
sidouxie <sid_ouxiee@hotmail.com>
2020-06-28 18:54:01 +0100

    pagination des episodes résolu :)

--- components/Player.js
+++ components/Player.js
@@ -2,17 +2,29 @@ import React,{useState,useEffect} from 'react';
 
 
 
-const Player = props => {
-    const [Active, setActive] = useState('jak aissa');
+const Player = ({serie}) => {
+    const [Count, setCount] = useState(0);
     useEffect(() => {
-
+        console.log(Count);
+        
     },[])
 
     return (
         <div>
-            <h1 onClick={() => setActive(Active)}>Data est : Bonjours {Active} </h1>
-            {props.children}
+                <div classNasme="section-info">
+                    <div className="section-one">
+                        <h3>Serie : {serie.title} en VF </h3>
+                        <h3>Saison : {serie.saisons.saison.title} </h3>
+                    </div>
+                        <div><ul>
+                            {serie.saisons.saison.episodes.map(({ title, id, url }) => (<li key={id} onClick={() => setCount(id)}><h3>{title}</h3></li>))} 
+                        </ul></div>
+                    </div>
+                <div className="section-vid">
+                    <iframe style={{width: "100%"}} src={`https://embed.mystream.to/${serie.saisons.saison.episodes[Count - 1].url}`} scrolling="no" frameBorder="0" width="700" height="420" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>    
+                </div>
         </div>
+        
     )
 }
 
--- pages/series/[slug].js
+++ pages/series/[slug].js
@@ -122,23 +122,8 @@ export default ({ filmes, series, serie, count }) => (
                     </div>
                 </div>
             </div>
-            <div>
-                <div className="section-info">
-                    <div className="section-one">
-                        <h3>Serie : {serie.title} en VF </h3>
-                        <h3>Saison : {serie.saisons.saison.title} </h3>
-                    </div>
-                        
-                        <div><ul>
-                            {serie.saisons.saison.episodes.map(({ title, id, url }) => (<li key={id} onClick={() => setCount()} ><h3>{title}</h3></li>))} 
-                        </ul></div>
-
-                </div>
-                <div className="section-vid">
-                    <iframe style={{width: "100%"}} src={`https://embed.mystream.to/${serie.saisons.saison.episodes[0].url}`} scrolling="no" frameBorder="0" width="700" height="420" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>    
-                </div>
-                </div>
-                <Player />
+            
+            <Player serie={serie} />
             </Layout>
             </FilmesContext.Provider>
         </React.Fragment>
