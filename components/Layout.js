db5ee56f99af19993b732194c6e5369d915ba896
sidouxie <sid_ouxiee@hotmail.com>
2020-07-15 00:43:15 +0100

    nouveau logo, favicon

--- components/Layout.js
+++ components/Layout.js
@@ -3,13 +3,14 @@ import Nav from './Nav';
 import Head from 'next/head';
 
 
-
-
 const Layout = (props) => {
         return (
         <>
             <Head>
-                <title>Droppy - site streaming VF Filmes, Séries, Animes en francais gratuit.</title>
+                <title>Droppy - site streaming VF Filmes, Séries, Animes en français gratuit.</title>
+                    <link rel="apple-touch-icon" sizes="180x180" href='/static/droppy_favicon.ico' ></link>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/droppy_favicon.ico" ></link>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/static/droppy_favicon.ico" ></link>
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
             </Head>
 
--- components/Pagination.js
+++ components/Pagination.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Pagination({ filmePerPage, totalFilmes, paginate }) {
     const pageNumbers = [];
-
+    
     for (let i = 1; i <= Math.ceil(totalFilmes / filmePerPage); i++) {
         pageNumbers.push(i);
     }
@@ -12,7 +12,7 @@ function Pagination({ filmePerPage, totalFilmes, paginate }) {
             <ul style={{justifyContent:'center'}} className='pagination'>
                 {pageNumbers.map(number => (
                     <li key={number} className='page-item'>
-                        <button onClick={() => paginate(number)} className='page-link'>{number}</button>
+                        <button onClick={() => paginate(number)} className="page-link">{number}</button>
                         
                     </li>
                 ))}
