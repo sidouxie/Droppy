c117fdcf54cd6d99d2464445eea83fe84c8ae836
sidouxie <sid_ouxiee@hotmail.com>
2020-07-22 17:16:24 +0100

    Ajoute du Smart Menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Link from 'next/link';
 import SearchBar from './SearchBar';
 import { QueryContext } from './FilmesContext';
@@ -10,11 +10,32 @@ function Menu(props) {
     const [Active, setActive] = useState(false);
     const [Hover, setHover] = useState(false);
 
+    const [Showed, setShowed] = useState(true);
+    const [ScrollPos, setScrollPos] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            isChanging();
+        }
+    }, [ScrollPos])
+
+    const handleScroll = ()  => {
+        setScrollPos(document.body.getBoundingClientRect().top)
+        setShowed({isWeed : document.body.getBoundingClientRect().top > ScrollPos})
+    }
+    
+    function isChanging() {
+        const changing = Showed.isWeed;
+        return changing ? 'active' : 'hidden';
+    }
+
     const Data = useContext(QueryContext);
 
     return (
         <>
-            <header>
+            <header className={`header-menu ${isChanging()}`}>
                 <nav>
                     <div className="section-recherche">
                         <span onClick={() => setActive(!Active)} className="icon-search"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.8 20.8" width='25' height='25' onMouseEnter={()=> setHover(!Hover)} onMouseLeave={()=>setHover(!Hover)} fill={Hover ? '#fff' : '#b2b2b2'}><title>search-icon</title><path d="M14.42,13A8,8,0,1,0,13,14.42l6.48,6.48.26,0a1.32,1.32,0,0,0,.46-.2,1.42,1.42,0,0,0,.63-.9l.06-.27ZM8.1,2.1a6,6,0,1,1-6,6A6,6,0,0,1,8.1,2.1Z" transform="translate(-0.1 -0.1)"/></svg></span>
--- components/Pagination.js
+++ components/Pagination.js
@@ -1,10 +1,17 @@
 import React,{useState} from 'react'
+import { number } from 'prop-types';
 
 function Pagination({ filmePerPage, totalFilmes, paginate }) {
     const pageNumbers = [];
+    const [Active, setActive] = useState(1);
     
     for (let i = 1; i <= Math.ceil(totalFilmes / filmePerPage); i++) {
         pageNumbers.push(i);
+    }
+
+    function isActive(number) {
+        const select = Active === number;
+        return select ? 'active' : '';
     }
 
     return (
@@ -12,7 +19,7 @@ function Pagination({ filmePerPage, totalFilmes, paginate }) {
             <ul style={{justifyContent:'center'}} className='pagination'>
                 {pageNumbers.map(number => (
                     <li key={number} className='page-item'>
-                        <button onClick={() => paginate(number)} className="page-link">{number}</button>
+                        <button onClick={() => {paginate(number), setActive(number)}} className={`page-link ${isActive(number)}`}>{number}</button>
                         
                     </li>
                 ))}
