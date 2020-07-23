341aa8673c75ad6038120989d559d3bb6cba845e
sidouxie <sid_ouxiee@hotmail.com>
2020-07-23 17:57:03 +0100

    oh putin de player a la con

--- components/PlayerFilme.js
+++ components/PlayerFilme.js
@@ -26,11 +26,11 @@ const PlayerFilme = ({filme}) => {
             </AdBlockDetect>
 
             <div className={`section-player ${isChange(1)}`}>
-                <iframe style={{width: "100%"}} src={`https://embed.mystream.to/${filme.apiurl}`} scrolling="no" frameBorder="0" width="700" height="340" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>    
+                <iframe style={{width: "100%"}} src={`https://embed.mystream.to/${filme.apiurl}`} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" width="700" height="340" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>    
             </div>
 
             <div className={`section-player-up ${isChange(2)}`}>
-                <iframe style={{width: '100%'}} src={`https://upstream.to/embed-${filme.urlCode}.html`} scrolling="no" frameBorder="0" width="700" height="340" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+                <iframe style={{width: '100%'}} src={`https://upstream.to/embed-${filme.urlCode}.html`} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" width="700" height="340" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
             </div>
         
             <div className="change-player">
