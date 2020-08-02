f141006fc09aa183be29a49f8a3d414e9262ec39
sidouxie <sid_ouxiee@hotmail.com>
2020-08-03 00:31:31 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 import Carousel from 'react-multi-carousel';
 import { QueryContext } from '../components/FilmesContext'
 
-// Ajoute des filme
+// ajout 03/08 2 filme
 
 
 export async function getStaticProps() {
