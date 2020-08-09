f1058e514acecb219ffa80c22f76db7323a87a32
sidouxie <sid_ouxiee@hotmail.com>
2020-08-09 14:30:26 +0100

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
+        {/* <!-- Google Analytics --> */}
+          <script dangerouslySetInnerHTML={{__html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-136464905-5', 'auto');
+          ga('send', 'pageview');`}} />
+          
+        {/* <!-- End Google Analytics --> */}
+
         {/* <!-- Google Tag Manager --> */}
           <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
