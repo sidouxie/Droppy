170cace5dcfb295cbbda03006fe56d2791d81ef1
sidouxie <sid_ouxiee@hotmail.com>
2020-08-11 20:44:36 +0100

    update next js & elliptic

--- components/Player.js
+++ components/Player.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { parseCookies, setCookie, destroyCookie } from 'nookies';
+import React, { useState } from 'react';
+import { parseCookies, setCookie } from 'nookies';
 import AdBlockDetect from 'react-ad-block-detect';
 import PropTypes from 'prop-types';
 
