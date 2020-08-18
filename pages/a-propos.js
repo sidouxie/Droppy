52bb19d1336fa14bdbbbbdd570d85f65ae2cfb20
sidouxie <sid_ouxiee@hotmail.com>
2020-08-19 00:51:32 +0100

    ajoute de la page a propos

--- pages/a-propos.js
+++ pages/a-propos.js
@@ -52,11 +52,43 @@ const Apropos = ({ filmes, series }) => {
                 <Head>
                     <title>Droppy - à propos.</title>
                 </Head>
-            <div className="container-fluid">
-                <div className="row">
-                    <h1>Bienvenue sur la page A propos.</h1>
-                </div>
+
+                <div className="section-head-about">
+                    <div className="about-title">
+                        <h1>Plateforme de streaming la plus légère</h1>
+                    </div>
             </div>
+            <div className="container">
+                <div className="row">
+                        <div className="section-textil">
+                            <div className="section-textil-info" style={{color: 'rgb(179, 181, 191)'}}>
+                                <span>Conçu avec les dernières technos qui existe en ce moment, Droppy offre à ces web spectateurs le meilleur en matière d'expérience utilisateur pour passer un agréable moment.</span>
+                            </div>
+                            
+                            <h3>Mon histoire avec Droppy</h3>
+                            
+                            <div className="textil">
+                                <p>Au temps du confinement comme tout le monde j’ai passé mes journées à regarder des filmes et séries sur des sites de streaming gratuits, mais souvent le souci avec ces sites c’est que la majorité des temps ils sont en manque d’optimisation ou souvent mal codé ou ces pop-up qui sortent de nulle part chaque seconde ou ont cliqué et leur sécurité qui laisse à désirer.</p>
+                                <p>Donc c’est là où j’ai eu l’idée de créer une plateforme qui soit rapide et simple, et le point le plus important qu’elle soit sécurisé.</p>
+                                <p>Passionné depuis mon plus jeun âge par le monde du cinéma et du digital j’ai décidé de mettre tout mon savoir fair pour mettre en place une plateforme innovante et qui propose quelque chose de nouveau.</p>
+                            </div>
+                    </div>
+                </div>
+                </div>
+                <div className="section-head-about-second">
+                    <div className="about-title">
+                        <h2>Pourquoi choisir de regarder sont filme préférée sur Droppy plutôt que sur d’autre sites ?</h2>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="section-textil">
+                            <div className="section-textil-info" style={{color: 'rgb(179, 181, 191)'}}>
+                                <span>Excellent question cher cinéphile, car vous mettrez moins de temps pour commencer à regarder votre film sur Droppy que pour charger la page d'accueil de quelques sites streaming.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Layout>
             </QueryContext.Provider>
     )
