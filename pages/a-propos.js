4b07ade944f7e0ef2a3601a22848894673256f1e
sidouxie <sid_ouxiee@hotmail.com>
2020-08-20 19:49:29 +0100

    dinner presque parfait

--- pages/a-propos.js
+++ pages/a-propos.js
@@ -69,9 +69,9 @@ const Apropos = ({ filmes, series }) => {
                             <span className="subtitle">Le storytalling ce n'est pas mon fort encore moins le copywriters.</span>
                             
                             <div className="textil">
-                                <p>Au temps du confinement comme tout le monde j’ai passé mes journées à regarder des filmes et séries sur des sites de streaming gratuits, mais souvent le souci avec ces sites c’est que la majorité des temps ils sont en manque d’optimisation ou souvent mal codé ou ces pop-up qui sortent de nulle part chaque seconde ou ont cliqué et leur sécurité qui laisse à désirer.</p>
-                                <p>Donc c’est là où j’ai eu l’idée de créer une plateforme qui soit rapide et simple, et le point le plus important qu’elle soit sécurisé.</p>
-                                <p>Passionné depuis mon plus jeun âge par le monde du cinéma et du digital j’ai décidé de mettre tout mon savoir fair pour mettre en place une plateforme innovante et qui propose quelque chose de nouveau.</p>
+                                <p>Au temps du confinement comme tout le monde j’ai passé mes journées à regarder des filmes et séries sur des sites de streaming gratuits, mais souvent le souci avec ces sites c’est que la majorité du temps ils sont en manque d’optimisation ou souvent mal codés et le point qui ma tourner le cerveau c'est tout ces pop-up qui sortent de nulle part partout ou on clique sur le site et sans parler de leur sécurité qui laisse à désirer.</p>
+                                <p>Donc c’est là où j’ai eu l’idée de créer une plateforme qui soit rapide et simple à utiliser, et le point le plus important qu’elle soit sécurisé.</p>
+                                <p>Passionné depuis mon plus jeun âge par le monde du cinéma et du digital j’ai décidé de mettre tout mon savoir fair pour mettre en place une plateforme innovante et qui propose des services dignes des plus grandes plateformes de streaming.</p>
                             </div>
                     </div>
                 </div>
@@ -88,8 +88,81 @@ const Apropos = ({ filmes, series }) => {
                                 <span>Excellent question cher cinéphile, car vous mettrez moins de temps pour commencer à regarder votre film sur Droppy que pour charger la page d'accueil de quelques sites streaming.</span>
                             </div>
                         </div>
+                        <div className="mobile-responsive">
+                            <img src="static/iphone-png-2.png" alt="iphone mobile responsive"/>
+                        </div>
+        
                     </div>
                 </div>
+
+                <div className="section-head-about-quality">
+                    <div className="about-title">
+                        <h2>Les qualités de la plateforme</h2>
+                    </div>
+                </div>
+
+                <div className="section-quality">
+                            <div className="bloc-info">
+                                <span className="icon"><img src="static/icone-1.svg" alt="icone svg"/></span>
+                                <div className="title">
+                                    <h3>Contenu exclusive</h3>
+                                    <p className="subtitle">
+                                    Le contenu est 100% francophone et en toute exclusivité, la majorité du temps en qualité DVD ou 720p.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bloc-info">
+                                <span className="icon"><img src="static/icone-2.svg" alt="icone svg"/></span>
+                                <div className="title">
+                                    <h3>Sans Pop-up</h3>
+                                    <p className="subtitle">
+                                    Ne pas vous attendre à être bombardé de pop-up en arrivant sur le site.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bloc-info">
+                                <span className="icon"><img src="static/icone-3.svg" alt="icone svg"/></span>
+                                <div className="title">
+                                    <h3>Visionnage gratuit</h3>
+                                    <p className="subtitle">
+                                    Le visionnage du contenu est complètement gratuit et le restera pour toujours.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bloc-info">
+                                <span className="icon"><img src="static/icone-4.svg" alt="icone svg"/></span>
+                                <div className="title">
+                                    <h3>Mises à jours</h3>
+                                    <p className="subtitle">
+                                    Droppy évolue chaque jour et reçoit une maintenance journalière pour garder la sécurité et la performance de la plateforme.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bloc-info">
+                                <span className="icon"><img src="static/icone-5.svg" alt="icone svg"/></span>
+                                <div className="title">
+                                    <h3>Expérience utilisateur</h3>
+                                    <p className="subtitle">
+                                    Une interface simple et moderne pour faciliter l’expérience utilisateur et la rendre agréable.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bloc-info">
+                                <span className="icon"><img src="static/icone-6.svg" alt="icone svg"/></span>
+                                <div className="title">
+                                    <h3>Recherche simplifié</h3>
+                                    <p className="subtitle">
+                                    Droppy vous offre une barre de recherche performante pour trouver votre film préféré rapidement.
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
             </Layout>
             </QueryContext.Provider>
     )
