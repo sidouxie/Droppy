8b0950448fb0c17c15bdd650508e915ef973a8b9
sidouxie <sid_ouxiee@hotmail.com>
2020-08-23 18:02:36 +0100

    .

--- components/Footer.js
+++ components/Footer.js
@@ -15,8 +15,8 @@ function Footer() {
                 </div>
                 <span className="ligne" ></span>
 
-                <span className="footer-text"><span style={{fontWeight:'600'}}>Droppy ™ 2020</span>, Designer <a target = "_blank" href="https://www.xn--changedeliens-9gb.com" >&</a> <a target = "_blank" href="http://www.boostersite.com" ><img style={{width:"10px"}} src="/static/boostersite-logo.gif" alt="boostersite logo"/></a>codé avec <a target="_blank" href="http://www.robothumb.com">💙</a></span>
-                
+                <span className="footer-text"><span style={{fontWeight:'600'}}>Droppy ™ 2020</span>, Designer <a target = "_blank" href="https://www.xn--changedeliens-9gb.com" >&</a> <a target = "_blank" href="http://www.boostersite.com" ><img style={{width:"auto"}} src="/static/boostersite-logo.gif" alt="boostersite logo"/></a>codé avec <a target="_blank" href="http://www.robothumb.com">💙</a></span>
+                <span className="footer-text"><a target = "_blank" href="http://www.fresh-annuaire.com/streaming.html">Annuaire streaming</a> <a href="http://www.net-liens.com" target="_blank"><img src="http://www.net-liens.com/img/media/nl1.jpg" alt="Net Liens - L'annuaire Internet" title="Net Liens - L'annuaire Internet" /></a></span>
                 <span className="footer-logo"><img src='/static/new-droppy.svg' alt='Droppy logo' style={{ width:'70px'}} /></span>
 
             </div>
